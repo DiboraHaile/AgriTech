e0124fb22be58b96fb236b39a6da85e037012beb
DiboraHaile <deborahaile3@gmail.com>
2021-08-18 20:47:14 +0300

    add data fetching functions

---
+++ scripts/data_fetcher.py
@@ -0,0 +1,34 @@
+import json
+import pdal 
+import os
+
+class FetchData():
+    def __init__(self,public_data_path,region,bounds,pipeline_def):
+        self.pipeline_df = pipeline_def
+        self.public_data_path = public_data_path
+        self.region = region
+        self.public_access_path = self.public_data_path + self.region + "ept.json",
+        self.bounds = bounds
+        self.pipeline_def = pipeline_def
+        self.output_file = "data.geojson"
+        self.set_pipeline_df()
+        self.execute_pipeline()
+
+    def set_pipeline_df(self):
+        self.pipeline_def[-1]["filename"] = self.output_file
+        self.pipeline_def[0]['filename'] = self.public_access_path
+        print(self.pipeline_def)
+        # pipeline_def[0]['bounds'] = bounds
+        pipeline_json =  json.dumps(self.pipeline_def)
+        self.pipeline = pdal.Pipeline(pipeline_json)
+
+    def execute_pipeline(self):
+        if os.path.exists(self.output_file):
+            os.remove(self.output_file)
+            
+        count = self.pipeline.execute()
+        data = open(self.output_file, 'r').read()
+        data = data.replace("[,{", "[{").replace("}{", "},{")
+        open(self.output_file, 'w').write(data)
+
+        
