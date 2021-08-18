c57bbb2be7d35111f9c6d3066cc7bac78db56ed0
DiboraHaile <deborahaile3@gmail.com>
2021-08-18 22:43:17 +0300

    modify functions

--- scripts/data_fetcher.py
+++ scripts/data_fetcher.py
@@ -1,24 +1,26 @@
 import json
 import pdal 
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join('../')))
 
 class FetchData():
     def __init__(self,public_data_path,region,bounds,pipeline_def):
         self.pipeline_df = pipeline_def
         self.public_data_path = public_data_path
         self.region = region
-        self.public_access_path = self.public_data_path + self.region + "ept.json",
+        self.public_access_path = self.public_data_path + self.region + "ept.json"
         self.bounds = bounds
         self.pipeline_def = pipeline_def
-        self.output_file = "data.geojson"
+        self.output_file = "data/data.geojson"
         self.set_pipeline_df()
         self.execute_pipeline()
 
     def set_pipeline_df(self):
-        self.pipeline_def[-1]["filename"] = self.output_file
+        # self.pipeline_def[-1]["filename"] = self.output_file
         self.pipeline_def[0]['filename'] = self.public_access_path
         print(self.pipeline_def)
-        # pipeline_def[0]['bounds'] = bounds
+        self.pipeline_def[0]['bounds'] = self.bounds
         pipeline_json =  json.dumps(self.pipeline_def)
         self.pipeline = pdal.Pipeline(pipeline_json)
 
@@ -26,7 +28,7 @@ class FetchData():
         if os.path.exists(self.output_file):
             os.remove(self.output_file)
             
-        count = self.pipeline.execute()
+        self.pipeline.execute()
         data = open(self.output_file, 'r').read()
         data = data.replace("[,{", "[{").replace("}{", "},{")
         open(self.output_file, 'w').write(data)
